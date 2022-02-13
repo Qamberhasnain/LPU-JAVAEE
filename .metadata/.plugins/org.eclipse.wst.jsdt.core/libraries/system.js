2c554621f01c944924cd2f38c404c9d849ec3535
Qamberhasnain <qamber.qh786@gmail.com>
2022-02-13 20:46:23 +0530

    SessionTill19

--- .metadata/.plugins/org.eclipse.wst.jsdt.core/libraries/system.js
+++ .metadata/.plugins/org.eclipse.wst.jsdt.core/libraries/system.js
@@ -1,9 +1,9 @@
 /*******************************************************************************
  * Copyright (c) 2008, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
+ * are made available under the terms of the Eclipse Public License v2.0
  * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * https://www.eclipse.org/legal/epl-2.0/
  *
  * Contributors:
  *     IBM Corporation - initial API and implementation
@@ -477,14 +477,14 @@ Array.prototype.sort = function(funct){return [];};
 Array.prototype.splice = function(start, deletecount, items){return [];};
 /**
   * function unshift(items)
-  * @param {Array} start
-  * @returns {Array}
+  * @param {Object} values
+  * @returns {Number}
   * @memberOf   Array
   * @see     Array
   * @since   Standard ECMA-262 3rd. Edition 
   * @since   Level 2 Document Object Model Core Definition.   
  */  
-Array.prototype.unshift = function(start){return [];};
+Array.prototype.unshift = function(values){return 1;};
 
 /**
   * Object Function()
